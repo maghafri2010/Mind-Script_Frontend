04f075550d3bca8806ea5707d05efac70b0eb38d
Maghafri2010 <mabdo@MABDO.localdomain>
2025-07-10 02:56:23 +0100

    setting the home page design <desktop>

--- frontend/src/home/header.jsx
+++ frontend/src/home/header.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router';
-
+import logo from '../assets/images';
 const Header = () => {
 
     return (
         <div className="flex bg-[#FAC9B3] fixed h-20 w-full mt-5 items-center justify-between">
-            <div>
-                <img src="" alt="" />
+            <div className='flex items-center gap-4 ml-6'>
+                <img className='border w-20 h-16' src={logo} alt="" />
                 <h1>Mind Script</h1>
             </div>
             <div>
